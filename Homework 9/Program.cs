baaef6da2bfde982abc33d60e8fac25290617a51
Davud <davud.aliyev2007@gmail.com>
2024-01-09 13:58:59 +0400

    Add project files.

---
+++ Homework 9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Doğum tarixinizi daxil etdiyinizdə sizə yaşınızı qaytaran metod
+
+            DateTime birthDate = new DateTime(2007, 8, 26);
+            var age = (DateTime.Now.Year-birthDate.Year);
+
+            Console.WriteLine(age);
+        }
+    }
+}
